2bb5f3b60f76f25f85688b62f2b676ef1060e3f2
unknown <Rob@C28258.chelt.local>
2020-06-12 15:16:26 +0100

    update

--- Scripts/WIMD_Pre-processing.R
+++ Scripts/WIMD_Pre-processing.R
@@ -1,3 +1,6 @@
+#> This code joins the stats tables in the project folder "Data > In > 2_WIMD_Stats_CSVs" to the spatial data for LSOAs in "Data > In > 1_Shapefiles > LSOA_Wales_2011" and combines then into a single (spatial) dataset ready for analysis and visulisation in QGIS
+
+#> Libraries
 library(tidyverse)
 library(here)
 library(sf)
